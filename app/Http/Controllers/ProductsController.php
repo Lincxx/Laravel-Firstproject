a9c2362cb4ee7bab78d525dfc772a7a1b061e55b
Lincxx <bossco28@gmail.com>
2022-03-17 06:48:59 -0400

    Added controllers and different ways to pass data to the view

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index() {
+        $title = "Welcome to my Laravel 8 course";
+        $description = "Create by Jeremy";
+
+        $data = [
+            'product1' => 'iPhone',
+            'product2' => 'Samsung',
+        ];
+
+        // Compact method
+        // return view('products.index', 
+        //              compact('title', 'description'
+        //         ));
+
+        //With method - good for one variable, or an array 
+        // return view('products.index')->with('title', $title);
+        //return view('products.index')->with('data', $data);
+
+        // Directly in the view
+        return view('products.index', [
+            'data' => $data
+        ]);
+
+    }
+
+    public function about() {
+        return "About page";
+    }
+}
